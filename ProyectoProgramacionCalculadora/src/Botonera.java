385840e8d9f0a927c4d43fbfbac5940a38339850
Mario <mmatamala14@alumnos.utalca.cl>
2018-05-10 21:24:17 -0300

    botones parte cientifica
    se agregaron el teclado cientifico ( ! y ^ )

--- ProyectoProgramacionCalculadora/src/Botonera.java
+++ ProyectoProgramacionCalculadora/src/Botonera.java
@@ -131,23 +131,7 @@ public class Botonera {
         //------------------------------//
        
         cajaDeSimbolos.getChildren().addAll(simbolos1,simbolos2);
-        
-        VBox trigonometria = new VBox();
-        trigonometria.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
-        Button buttonSen= new Button("  sen  ");
-        Button buttonCos = new Button("  cos  ");// en este caso el contenido se entrega en el contructor
-        Button buttonTan = new Button("  tan  ");// en este caso el contenido se entrega en el contructor
-        HBox.setHgrow(buttonSen, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
-        HBox.setHgrow(buttonCos, Priority.ALWAYS);
-        HBox.setHgrow(buttonTan, Priority.ALWAYS);
-        buttonSen.setMaxWidth(Double.MAX_VALUE);
-        buttonCos.setMaxWidth(Double.MAX_VALUE);
-        buttonTan.setMaxWidth(Double.MAX_VALUE);
-        
-        trigonometria.getChildren().addAll(buttonSen, buttonCos, buttonTan);
-        trigonometria.getChildren().removeAll(buttonSen, buttonCos, buttonTan);
-        cajaDeSimbolos.getChildren().addAll(trigonometria);
-        
+               
        HBox simbolos4 = new HBox();
        simbolos4.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
        Button buttonNn2 = new Button(" Esconder Puntos de Control ");
@@ -173,26 +157,177 @@ public class Botonera {
         HBox.setHgrow(simbolos1, Priority.ALWAYS);
         HBox.setHgrow(simbolos2, Priority.ALWAYS);
        
-        /*
-        button7.setOnAction((ActionEvent event) ->
-
-         {
-             double n =0;
-             NumerosYSimbolos num7 = new NumerosYSimbolos(n, pantalla.getEspacioNumero(),pantalla.getEspacioSuperior(), pantalla.isPuntosVisibles());
-             pantalla.agregarACentro(num7.numero7());
-             pantalla.contador(false);
-             pantalla.addEnPantalla(num7);
-             pantalla.tryDivide();
-          });
-          */
+        
        return contenerdorPrincipal;
        
     }
     
     HBox cientifica(){
         HBox contenerdorPrincipal = new HBox();
-        Button button7 = new Button(" 7 ");
-        contenerdorPrincipal.getChildren().add(button7);
+        
+        HBox contenedorNumeros = new HBox();
+       VBox numerosColumna1 = new VBox();//creamos el Hbox
+       numerosColumna1.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
+       Button button7 = new Button(" 7 ");
+       Button button4 = new Button(" 4 ");// en este caso el contenido se entrega en el contructor
+       Button button1 = new Button();
+       Button buttonBajar = new Button("Bajar");
+       button1.setText(" 1 ");//contenido del boton 
+       button1.setMaxWidth(Double.MAX_VALUE);
+      
+       //HBox.setMargin(button1, new Insets(0,0,0,0));//margen entre objetos en este caso se dejo 5 a la  derecha 
+       HBox.setHgrow(button7, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
+       HBox.setHgrow(button4, Priority.ALWAYS);
+       HBox.setHgrow(button1, Priority.ALWAYS);
+       HBox.setHgrow(buttonBajar, Priority.ALWAYS);
+       HBox.setMargin(button7, javafx.geometry.Insets.EMPTY);
+       button7.setMaxWidth(Double.MAX_VALUE);
+       button4.setMaxWidth(Double.MAX_VALUE);
+       buttonBajar.setMaxWidth(Double.MAX_VALUE);
+       numerosColumna1.getChildren().addAll(button7, button4,button1,buttonBajar);
+       
+       
+       //------------------------------//
+       
+        VBox numerosColumna2 = new VBox();
+       numerosColumna2.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
+       Button button8 = new Button(" 8 ");
+       Button button5 = new Button(" 5 ");// en este caso el contenido se entrega en el contructor
+       Button button2 = new Button(" 2 ");
+       Button button0 = new Button(" 0 ");
+       HBox.setHgrow(button8, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
+       HBox.setHgrow(button5, Priority.ALWAYS);
+       HBox.setHgrow(button2, Priority.ALWAYS);
+       HBox.setHgrow(button0, Priority.ALWAYS);
+       button8.setMaxWidth(Double.MAX_VALUE);
+       button5.setMaxWidth(Double.MAX_VALUE);
+       button2.setMaxWidth(Double.MAX_VALUE);
+       button0.setMaxWidth(Double.MAX_VALUE);
+       numerosColumna2.getChildren().addAll(button8, button5,button2,button0);
+       
+       //------------------------------//
+       
+       VBox numerosColumna3 = new VBox();
+       numerosColumna3.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
+       Button button9 = new Button(" 9 ");
+       Button button6 = new Button(" 6 ");// en este caso el contenido se entrega en el contructor
+       Button button3 = new Button(" 3 ");
+       Button buttonEliminar = new Button(" CE ");
+       HBox.setHgrow(button9, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
+       HBox.setHgrow(button6, Priority.ALWAYS);
+       HBox.setHgrow(button3, Priority.ALWAYS);
+       HBox.setHgrow(buttonEliminar, Priority.ALWAYS);
+       button9.setMaxWidth(Double.MAX_VALUE);
+       button6.setMaxWidth(Double.MAX_VALUE);
+       button3.setMaxWidth(Double.MAX_VALUE);
+       buttonEliminar.setMaxWidth(Double.MAX_VALUE);
+       numerosColumna3.getChildren().addAll(button9, button6,button3,buttonEliminar);
+       
+   
+       //------------------------------//
+       
+       contenedorNumeros.getChildren().addAll(numerosColumna1,numerosColumna2,numerosColumna3);
+       
+       //*******fin numeros***************
+       
+       //*******inicio simbolos***********
+       
+       VBox contenedorSimbolos = new VBox();
+       //------------------------------//
+       //contenedor de los simbolos
+         HBox cajaDeSimbolos = new HBox();
+        
+       
+       //------------------------------//
+       VBox simbolos1 = new VBox();
+       simbolos1.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
+       Button buttonPar1 = new Button("   (   ");
+       Button buttonMas = new Button(" + ");// en este caso el contenido se entrega en el contructor
+       Button buttonPor = new Button(" * ");
+       HBox.setHgrow(buttonPar1, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
+       HBox.setHgrow(buttonMas, Priority.ALWAYS);
+       HBox.setHgrow(buttonPor, Priority.ALWAYS);
+       buttonPar1.setMaxWidth(Double.MAX_VALUE);
+       buttonMas.setMaxWidth(Double.MAX_VALUE);
+       buttonPor.setMaxWidth(Double.MAX_VALUE);
+      
+       simbolos1.getChildren().addAll(buttonPar1, buttonMas,buttonPor);
+       
+       //-------------------------------------//
+       
+       VBox simbolos2 = new VBox();
+       simbolos2.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
+       Button buttonPar2= new Button("  )  ");
+       Button buttonMenos = new Button(" - ");// en este caso el contenido se entrega en el contructor
+       Button buttonDiv = new Button(" / ");// en este caso el contenido se entrega en el contructor
+       HBox.setHgrow(buttonPar2, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
+       HBox.setHgrow(buttonMenos, Priority.ALWAYS);
+       HBox.setHgrow(buttonDiv, Priority.ALWAYS);
+       buttonPar2.setMaxWidth(Double.MAX_VALUE);
+       buttonMenos.setMaxWidth(Double.MAX_VALUE);
+       buttonDiv.setMaxWidth(Double.MAX_VALUE);
+      
+       simbolos2.getChildren().addAll(buttonPar2, buttonMenos,buttonDiv);
+       
+        //------------------------------//
+       
+        cajaDeSimbolos.getChildren().addAll(simbolos1,simbolos2);
+        
+        
+        VBox trigonometria = new VBox();
+        trigonometria.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
+        Button buttonSen= new Button("  sen()  ");
+        Button buttonCos = new Button("  cos()  ");// en este caso el contenido se entrega en el contructor
+        Button buttonTan = new Button("  tan()  ");// en este caso el contenido se entrega en el contructor
+        HBox.setHgrow(buttonSen, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
+        HBox.setHgrow(buttonCos, Priority.ALWAYS);
+        HBox.setHgrow(buttonTan, Priority.ALWAYS);
+        buttonSen.setMaxWidth(Double.MAX_VALUE);
+        buttonCos.setMaxWidth(Double.MAX_VALUE);
+        buttonTan.setMaxWidth(Double.MAX_VALUE);
+        
+        trigonometria.getChildren().addAll(buttonSen, buttonCos, buttonTan);
+        cajaDeSimbolos.getChildren().addAll(trigonometria);
+         
+        VBox simbolos = new VBox();
+        trigonometria.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
+        Button buttonGorrito= new Button("  ^  ");
+        Button buttonGrados = new Button("  °  ");// en este caso el contenido se entrega en el contructor
+        Button buttonFactorial = new Button("  !  ");// en este caso el contenido se entrega en el contructor
+        HBox.setHgrow(buttonGorrito, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
+        HBox.setHgrow(buttonGrados, Priority.ALWAYS);
+        HBox.setHgrow(buttonTan, Priority.ALWAYS);
+        buttonGorrito.setMaxWidth(Double.MAX_VALUE);
+        buttonGrados.setMaxWidth(Double.MAX_VALUE);
+        buttonFactorial.setMaxWidth(Double.MAX_VALUE);
+        simbolos.getChildren().addAll(buttonGorrito, buttonGrados, buttonFactorial);
+        cajaDeSimbolos.getChildren().addAll(simbolos);
+               
+       HBox simbolos4 = new HBox();
+       simbolos4.setPadding(new Insets(0));// se define el  margen entre el  borde del panel y  los objetos que estan dentro en pixeles 
+       Button buttonNn2 = new Button(" Esconder Puntos de Control ");
+       //Button buttonDiv = new Button(" / ");// en este caso el contenido se entrega en el contructor
+       
+       HBox.setHgrow(buttonNn2, Priority.ALWAYS);// esto  se define la prioridad  en caso de aumentar el tamaño de la ventana  los objetos con prioridad llenaran  el espacio 
+       //HBox.setHgrow(buttonDiv, Priority.ALWAYS);
+       buttonNn2.setMaxWidth(Double.MAX_VALUE);
+       //buttonDiv.setMaxWidth(Double.MAX_VALUE);
+      
+       simbolos4.getChildren().addAll(buttonNn2);
+       
+       //-------------------------------------//
+       
+
+        contenedorSimbolos.getChildren().addAll(cajaDeSimbolos,simbolos4);
+        contenerdorPrincipal.getChildren().addAll(contenedorNumeros,contenedorSimbolos);
+        HBox.setHgrow(contenedorNumeros, Priority.ALWAYS);// se define la prioridad de llenado de espacio que tiene el nodo  dentro de su contenedor.
+        HBox.setHgrow(contenedorSimbolos, Priority.ALWAYS);
+        HBox.setHgrow(numerosColumna1, Priority.ALWAYS);
+        HBox.setHgrow(numerosColumna2, Priority.ALWAYS);
+        HBox.setHgrow(numerosColumna3, Priority.ALWAYS);
+        HBox.setHgrow(simbolos1, Priority.ALWAYS);
+        HBox.setHgrow(simbolos2, Priority.ALWAYS);
+        
         return contenerdorPrincipal;
     }
 }
